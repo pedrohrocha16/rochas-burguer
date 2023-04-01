7df2d7e15c66e7ceee562eadc1d786f5abff7d88
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-04-01 08:07:32 -0300

    Corrigindo lógica

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 const controle = document.querySelectorAll('[data-controle]')
 
- controle.forEach( (elemento) => {
+controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
         quantidadeHamb(evento.target.dataset.controle, evento.target.parentNode) // identifica + ou - e o pai do elemento que foi clicado
         addQuantidadeHamb (evento.target.dataset.controles) // identifica qual produto foi clicado
     })
  })
     function quantidadeHamb(operacao, controle){
-        const qntd = controle.querySelector('[data-qntd]')
-        
+        const qntd = controle.querySelector('[data-qntd]') //dependendo da resposta (+-) ele aplica as condições
         if (operacao === '-'){
             qntd.value = Number(qntd.value) - 1
         } else {
             qntd.value = Number(qntd.value) + 1
         }
-    }
+        if (qntd.value < 0){
+            qntd.value = 0
+        }
+    }   
     
-
     function addQuantidadeHamb(){
-      
+        
     }
