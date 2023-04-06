200677253e4d74edffbc7c3f062b4f4b0e81a996
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-04-06 19:26:27 -0300

    Adicionando lógica

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-const removerProdutoscarrinho = document.querySelectorAll('.remove-btn')
+const removerProdutosCarrinho = document.querySelectorAll('.remove-btn')
 const finalizarCompra = document.querySelector('.btn-success')
 
-
+for (let i = 0; i < removerProdutosCarrinho.length; i++) {
+    removerProdutosCarrinho[i].addEventListener('click', function(event){
+        event.target.parentElement.parentElement.remove()
+    })
+    
+}
