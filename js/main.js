f25e3aef9dea26dbfe589d997df0dd473be4a53f
Pedro H. Rocha <pedro_6568@hotmail.com>
2023-04-08 10:48:29 -0300

    Alterando design quantidade e adicionando lógica

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 const removerProdutosCarrinho = document.querySelectorAll('.remove-btn')
 const finalizarCompra = document.querySelector('.btn-success')
 
+
 for (let i = 0; i < removerProdutosCarrinho.length; i++) {
-    removerProdutosCarrinho[i].addEventListener('click', function(event){
-        event.target.parentElement.parentElement.remove()
-    })  
+    removerProdutosCarrinho[i].addEventListener('click', removerProdutos)  
+}
+    const quantidadeProduto = document.querySelector
+
+
+
+
+
+
+function removerProdutos(event){
+    event.target.parentElement.parentElement.remove()
+        atualizarVlrCarrinho()
 }
 
-const produtosCarrinho = document.querySelectorAll('.produtos-carrinho')
 
-for (let i = 0; i<produtosCarrinho.length; i++) {
-    console.log(produtosCarrinho[i])
+function atualizarVlrCarrinho(){
+
+    let vlrTotalCarrinho = 0
+    const produtosCarrinho = document.querySelectorAll('.produtos-carrinho')
+    for (let i = 0; i<produtosCarrinho.length; i++) {
+        const productPrice = produtosCarrinho[i].querySelector('#preco-produto-carrinho').innerText.replace("R$","").replace(",", ".")
+        const productQntd = produtosCarrinho[i].querySelector('.controle-contador').value
+        
+        vlrTotalCarrinho += productPrice * productQntd
     
+    }
+    vlrTotalCarrinho = vlrTotalCarrinho.toFixed(2)
+    vlrTotalCarrinho = vlrTotalCarrinho.replace('.', ',')
+    document.querySelector(".vlrtotal-compra").innerText = "Total: R$ "+ vlrTotalCarrinho
+
 }
+
